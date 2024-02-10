3b61358cbc79d1a2780f4de6811fc7b34c31df31
prathamsheth773 <prathamsheth077@gmail.com>
2024-02-10 21:32:40 +0530

    props fooditems

--- ReactJs/first-app/src/App.js
+++ ReactJs/first-app/src/App.js
@@ -1,14 +1,38 @@
-import FirstComponent from './components/FirstComponent'
+// import FirstComponent from './components/FirstComponent'
 import './App.css';
-
-
+import FoodItem  from './components/FoodItem';
+let foodItem = [
+  {
+    name:'Veg Supreme Burger',
+    price: 99,
+    rating: 4.4,
+    img:'burger',
+  },
+  {
+    name:'Veg Supreme Pizza',
+    price: 299,
+    rating: 3.4,
+    img:'Pizza',
+  },
+  {
+    name:'Masala Dosa',
+    price: 149,
+    rating: 4.6,
+    img:'Dosa',
+  },
+]
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to react bootcamp</h1>
+      {/* <h1>Welcome to react bootcamp</h1>
       <FirstComponent title="CSS Masterclass"/>
-      <FirstComponent title="JavaScript Masterclass"/>
+      <FirstComponent title="JavaScript Masterclass"/> */}
+      <div className="container">
+      <FoodItem name ={foodItem[0].name} price = {foodItem[0].price} rating = {foodItem[0].rating} img = {foodItem[0].img}/>
+      <FoodItem/>
+      <FoodItem/>
 
+      </div>
     </div>
   );
 }
--- ReactJs/first-app/src/components/FoodItem.js
+++ ReactJs/first-app/src/components/FoodItem.js
@@ -1,3 +1,14 @@
-export default function FoodItem(){
-    
+export default function FoodItem(props)
+{
+    return(
+        <div className="food-card">
+            <div className="food-card-header">
+                <img src="https://source.unsplash.com/1600x800/?burger" alt="" className="food-card-img"/>
+                <p className="ratings">{props.rating}</p>
+            </div>
+            <h3>{props.name}</h3>
+            <h4>{props.price} RS</h4>
+            <p>Combination of exotic vegetables topped with onion, capsicum, mushroom tomato, black olives & baby</p>
+        </div>
+    )
 }
